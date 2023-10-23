47cdd9efe722973c281740acf9e5c6bcabaf3a6c
Annegreet <49638786+Annegreet@users.noreply.github.com>
2023-10-23 20:58:22 +0200

    Create 00_gridding_globnut.R

---
+++ 00_gridding_globnut.R
@@ -0,0 +1,49 @@
+## ---------------------------
+##
+## Script name: 
+##
+## Purpose of script: Gridding GlobNut database, reducing sampling bias
+##
+## Author: Annegreet Veeken
+##
+## Date Created: 2023-10-19
+##
+## Email: veeken.g.a@gmail.com
+##
+## ---------------------------
+##
+## Notes:
+##  
+## References:
+## - script based on dggridR vignette: https://cran.r-project.org/web/packages/dggridR/vignettes/dggridR.html
+## ---------------------------
+
+## Load packages
+if(!require(tidyverse)) install.packages("tidyverse")
+if(!require(sf)) install.packages("sf")
+if(!require(dggridR)) install.packages("dggridR")
+if(!require(rnaturalearth)) install.packages("rnaturalearth")
+if(!require(rnaturalearthdata)) install.packages("rnaturalearthdata")
+
+## Load data
+data_dir <- "Z:/_GLOBNUT1.0/" # directory with raw Globnut 1.0 data
+meta_env <- read.csv(paste0(data_dir, "Globnut1.0_env_variable_meta.csv"))
+globnut <- read.csv(paste0(data_dir, "Globnut1.0_metadata.csv")) %>% 
+  filter(!is.na(lat)|!is.na(lon)) 
+
+# Construct a global grid with cells approximately 1 km2 (res 16), meaning the distance between center of adjacent cells is ~0.6k
+dggs <- dgconstruct(res = 16, metric = TRUE) # see table for different resolutions https://github.com/r-barnes/dggridR
+
+# Get the corresponding grid cells for each plot
+globnut$cell <- dgGEO_to_SEQNUM(dggs,globnut$lon,globnut$lat)$seqnum
+
+# check the plot count by cell
+plotcounts  <- globnut %>% group_by(cell) %>% summarise(count = n())
+quantile(plotcounts$count)
+
+# get center coordinates of the cells
+gn_grid <- dgSEQNUM_to_GEO(dggs,globnut$cell) %>% 
+  data.frame(globnut$plot_ID, cell = globnut$cell, lon = .$lon_deg, lat = .$lat_deg) 
+saveRDS(gn_grid, "outputs/00_Globnut_grid_res15.rds")
+
+
